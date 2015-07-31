63135190d03d6368ea5d64baf22591a347fb5be5
Oasis <admin@acoolsip.com>
2015-07-31 03:57:11 -0500

    Update python_upgrade.sh

--- rhel6/python_upgrade.sh
+++ rhel6/python_upgrade.sh
@@ -1,10 +1,9 @@
 ```
 #!/bin/bash
-sudo yum install python
 sudo yum -y update
 sudo cd /usr/src
 sudo yum install gcc
-sudo yum install -y python-setuptools
+sudo yum install -y python python-setuptools python-pip
 sudo wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
 sudo tar xf Python-2.7.9.tgz
 sudo mv Python-2.7.9 python27
